37867b6d5ecc6acb71d085990da8ecee5fb1e9cf
mcxzrs <cc241047@fhstp.ac.at>
2026-01-28 18:57:50 +0100

    10 - README

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,5 +17,10 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
     globals: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      reportsDirectory: "./coverage",
+    },
   },
 })
